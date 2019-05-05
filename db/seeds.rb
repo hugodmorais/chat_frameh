980c613665d2382581fce3099e856582c82e7db3
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-05 12:50:53 +0100

    Add message partial and minor fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,9 @@ User.create!(username: "rodrimorais", password: "123456")
 User.create!(username: "soares", password: "123456")
 User.create!(username: "morais", password: "123456")
 User.create!(username: "keniasoares", password: "123456")
+
+
+Message.create!(body: "keniasoares fdghfdhghgf gfhgfjhgfjgf", user_id: "1")
+Message.create!(body: "fdghfdhghgf gfhgfjhgfjgf", user_id: "2")
+Message.create!(body: "keniasoares  gfhgfjhgfjgf", user_id: "3")
+Message.create!(body: "keniasoares", user_id: "4")
