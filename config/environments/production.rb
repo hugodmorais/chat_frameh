a3a7b6cd5c48d515d64d2eb331a9e6afdd64b82e
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-07 00:12:11 +0100

    minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = "wss://chatframeh.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://chatframeh.herokuapp.com', 'http://chatframeh.herokuapp.com']
+  config.serve_static_assets = true
 end
